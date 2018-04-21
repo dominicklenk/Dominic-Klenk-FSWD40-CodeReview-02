60edbca2ca1a66f8c6b3aab77599fe5171deb81a
dominicklenk <dominic_klenk@yahoo.at>
2018-04-21 16:55:52 +0200

    Done CodeReview2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var ProductOneName = "Stockholm";
+
+var ProductTwoName = "Madrid";
+
+var ProductOnePrice = 1300;
+
+var ProductTwoPrice = 1150;
+
+var Sum = ProductOnePrice + ProductTwoPrice;
+
+var Golden = Sum - (Sum / 10);
